985115acd6df50b42fbb2ac418e7ddde6a7741f4
FREEZ2385 <schlee9703@naver.com>
2020-10-09 22:09:33 +0900

    [Merge] get data from master branch

---
+++ src/CardPost.js
@@ -0,0 +1,18 @@
+export const CardPost = [
+    {
+        title : "Material Ui 전용 사이트 개설",
+        data : "2020-10-10",
+        content : "편하고 쉽게 인터페이스를 구성해주고 간편하게 수정이 가능한 편리한 기능!"
+    },
+    {
+        title : "Material Ui 전용 사이트 개설",
+        data : "2020-10-10",
+        content : "편하고 쉽게 인터페이스를 구성해주고 간편하게 수정이 가능한 편리한 기능!"
+    },
+    {
+        title : "Material Ui 전용 사이트 개설",
+        data : "2020-10-10",
+        content : "편하고 쉽게 인터페이스를 구성해주고 간편하게 수정이 가능한 편리한 기능!"
+    },
+]
+
