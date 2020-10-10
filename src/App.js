c87162da4f99054548f7a03a48054a3d66212b65
FREEZ2385 <schlee9703@naver.com>
2020-10-10 11:50:49 +0900

    [Update] Apply component in app.js

--- src/App.js
+++ src/App.js
@@ -3,15 +3,22 @@ import React from 'react';
 import './App.css';
 import AppBarComponent from './component/Organisms/AppBarComponent';
 import { CardPost } from './CardPost';
+import { ComponentGuideList } from './ComponentGuideList';
 import CardComponent from './component/Atoms/CardComponent';
+import ControlledAccordions from './component/Molecules/ControlledAccordions';
+import FloatingActionButtons from './component/Atoms/FloatingActionButtons';
 
 function App() {
   return (
     <div className="App">
       <AppBarComponent/>
-      <Container fullWidth='md' style={{ display: 'flex', marginTop: 20 }}>
-        {CardPost.map((data) => <CardComponent title={data.title} date={data.date} content={data.content}/>)}
+      <Container fullWidth='md' style={{ display: 'flex', marginTop: 20, justifyContent: 'center' }}>
+        {CardPost.map((data) => <CardComponent title={data.title} date={data.date} content={data.content} image={data.image}/>)}
       </Container>
+      <Container fullWidth='md' style={{ display: 'flex', marginTop: 20 }}>
+        <ControlledAccordions componentGuideList = {ComponentGuideList}/>
+      </Container>
+      <FloatingActionButtons/>
     </div>
   );
 }
--- src/CardPost.js
+++ src/CardPost.js
@@ -1,18 +1,25 @@
+import freez_logo  from './assets/img/freez_logo.jpg';
+import material_ui_logo from './assets/img/material_ui_logo.png';
+
+
 export const CardPost = [
     {
         title : "Material Ui 전용 사이트 개설",
         data : "2020-10-10",
-        content : "편하고 쉽게 인터페이스를 구성해주고 간편하게 수정이 가능한 편리한 기능!"
+        content : "편하고 쉽게 인터페이스를 구성해주고 간편하게 수정이 가능한 편리한 기능!",
+        image: material_ui_logo,
     },
     {
-        title : "Material Ui 전용 사이트 개설",
-        data : "2020-10-10",
-        content : "편하고 쉽게 인터페이스를 구성해주고 간편하게 수정이 가능한 편리한 기능!"
+        title : "구글 애드센스와 사이트 등록 완료",
+        data : "2020-10-01",
+        content : "구글 에드센스와 사이트 등록신청이 완료되었습니다. 이제 구글에서도 블로그 관련 포스트 검색이 가능합니다.",
+        image: "",
     },
     {
-        title : "Material Ui 전용 사이트 개설",
-        data : "2020-10-10",
-        content : "편하고 쉽게 인터페이스를 구성해주고 간편하게 수정이 가능한 편리한 기능!"
+        title : "프리즈의 귀차니즘 포스트 시작",
+        data : "2020-09-21",
+        content : "처음으로 블로그 포스트를 시작했습니다. 해당 링크는 https://freez2385.github.io/",
+        image: freez_logo,
     },
 ]
 
